f24123fc2f0623036fe71bb1b2b142d23c22d8e7
tawhidhaider <tawhidalam69@gmail.com>
2022-11-12 16:59:28 +0600

    service details

--- src/Pages/Services/ServiceDetails/ServiceDetails.js
+++ src/Pages/Services/ServiceDetails/ServiceDetails.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { PhotoProvider, PhotoView } from 'react-photo-view';
 import 'react-photo-view/dist/react-photo-view.css';
+import { useLoaderData } from 'react-router-dom';
 
-const ServiceCard = ({service}) => {
-    const {_id,title,price,details,image_url} = service;
+const ServiceDetails = () => {
+    const serviceDetails = useLoaderData();
+    console.log(serviceDetails);
+    const {_id,image_url,title,details,price} = serviceDetails;
     return (
         <div>
+            <div>
             <div className="max-w-xs rounded-md shadow-md bg-gray-50 text-gray-800">
                 <PhotoProvider>
                 <PhotoView src={image_url}>
@@ -17,25 +20,21 @@ const ServiceCard = ({service}) => {
                     <div className="space-y-2">
                         <h2 className="text-3xl font-semibold tracking-wide">{title}</h2>
                         <p className="text-gray-800">
-                            {details.length>100 ?
-                               <p>{details.slice(0,100)+ '...'}<Link to={`/services/${_id}`}>Read more</Link></p>
-                               :
-                               <p>{details}</p>
+                           
+                            {
+                                details
                             }
-                            
                             
                         </p>
                         <p>Price:${price}</p>
                     </div>
-                    <Link to={`/services/${_id}`}>
-                    <button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-indigo-400 text-gray-50">view-details</button>
-                    </Link>
+                   <button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-indigo-400 text-gray-50">view-details</button>
                 </div>
             </div>
+            </div>
             
-
         </div>
     );
 };
 
-export default ServiceCard;
+export default ServiceDetails;
--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Error from "../../Layout/Error";
 import Main from "../../Layout/Main";
 import Home from "../../Pages/Home/Home";
+import ServiceDetails from "../../Pages/Services/ServiceDetails/ServiceDetails";
 import Services from "../../Pages/Services/Services/Services";
 
 export const router = createBrowserRouter([
@@ -18,6 +19,11 @@ export const router = createBrowserRouter([
             {
                 path : '/services',
                 element :<Services></Services>
+            },
+            {
+                path :'/services/:id',
+                element :<ServiceDetails></ServiceDetails>,
+                loader :({params})=> fetch(`http://localhost:5000/services/${params.id}`)
             }
         ]
     }
